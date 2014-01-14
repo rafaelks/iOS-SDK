6f4e2bfad9b6ef9b18076a5f10b8d8168b865e79
Neel Patel and Sheel Choksi <pair+npatel+schoksi@sharethrough.com>
2014-01-14 10:29:34 -0800

    Initial commit

---
+++ SharethroughSDK/SharethroughSDK.h
@@ -0,0 +1,13 @@
+//
+//  SharethroughSDK.h
+//  SharethroughSDK
+//
+//  Created by sharethrough on 1/14/14.
+//  Copyright (c) 2014 Sharethrough. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SharethroughSDK : NSObject
+
+@end
---
+++ SharethroughSDK/SharethroughSDK.m
@@ -0,0 +1,13 @@
+//
+//  SharethroughSDK.m
+//  SharethroughSDK
+//
+//  Created by sharethrough on 1/14/14.
+//  Copyright (c) 2014 Sharethrough. All rights reserved.
+//
+
+#import "SharethroughSDK.h"
+
+@implementation SharethroughSDK
+
+@end
