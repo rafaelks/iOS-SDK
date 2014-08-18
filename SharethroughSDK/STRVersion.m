984dda0ac0cdd76022de0b16e76c23d31300fe7f
Mark Meyer <engineers+mmeyer@sharethrough.com>
2014-08-18 15:46:13 -0700

    Fix existing unit tests

--- SharethroughSDK/STRVersion.m
+++ SharethroughSDK/STRVersion.m
@@ -5,6 +5,8 @@
 //  Created by Engineer @editor.local on 8/18/14.
 //  Copyright (c) 2014 Sharethrough. All rights reserved.
 //
+// THIS VERSION IS UPDATED AT BUILD TIME.
+// ANY CHANGES TO THIS FILE WILL BE OVERWRITTEN
 
 #import "STRVersion.h"
 
