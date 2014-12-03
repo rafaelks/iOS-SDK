199f206747ff7d078f00f08f950509dcc09d8fd2
Mark Meyer <engineers+mmeyer@sharethrough.com>
2014-12-03 10:24:14 -0600

    STRAdvertisementSpecs

--- specs/Models/STRAdvertismentSpec.mm
+++ specs/Models/STRAdvertismentSpec.mm
@@ -1,4 +1,5 @@
 #import "STRAdvertisement.h"
+#import "STRImages.h"
 
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
@@ -19,12 +20,22 @@ describe(@"STRAdvertisement", ^{
 
     describe(@"-platformLogoForWidth", ^{
         it(@"returns a UIImageView with the Image set to the centerImage", ^{
+            char imageData[100];
+            [UIImagePNGRepresentation([ad platformLogoForWidth:100.0].image) getBytes:&imageData length:100];
+
+            char expectedData[100];
+            [UIImagePNGRepresentation([STRImages playBtn]) getBytes:&expectedData length:100];
+            imageData should equal(expectedData);
         });
 
         it(@"returns a view with a maximum size of the platform logo", ^{
+            UIImageView *logoView = [ad platformLogoForWidth:1000.0];
+            logoView.frame.size.width should equal([STRImages playBtn].size.width/2);
         });
 
         it(@"returns a view with a minimum size of 24", ^{
+            UIImageView *logoView = [ad platformLogoForWidth:10.0];
+            logoView.frame.size.width should equal(24);
         });
     });
 });
