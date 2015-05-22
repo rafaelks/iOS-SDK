77d34dd16ff2d85df9d7e6187bd11b17080662ce
Mark Meyer <akisaka@gmail.com>
2015-05-22 14:01:04 -0700

    Updated Podspec to 2.2.6. Added new logging header

---
+++ SharethroughSDK/Extension Helpers/STRLogging.h
@@ -0,0 +1,13 @@
+//
+//  STRLogging.h
+//  SharethroughSDK
+//
+//  Created by Mark Meyer on 5/22/15.
+//  Copyright (c) 2015 Sharethrough. All rights reserved.
+//
+
+#ifndef SharethroughSDK_STRLogging_h
+#define SharethroughSDK_STRLogging_h
+
+
+#endif
