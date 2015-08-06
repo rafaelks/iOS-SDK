f1bcc3d946c8fb94b02ecdc21cb90b84e694b8b4
Mark Meyer <mark.meyer37@gmail.com>
2015-08-06 17:50:26 -0500

    Short-circuit when there are no content rows
    * For table view and collection view
    * Return 0 if there are no real rows even if there are ads

--- SharethroughSDK/Proxies/STRGridlikeViewDataSourceProxy.m
+++ SharethroughSDK/Proxies/STRGridlikeViewDataSourceProxy.m
@@ -88,6 +88,9 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     NSInteger numberofContentRows = [self.originalTVDataSource tableView:tableView numberOfRowsInSection:section];
+    if (numberofContentRows == 0) {
+        return 0;
+    }
 
     self.numAdsInView = [self.adjuster numberOfAdsInSection:section givenNumberOfRows:numberofContentRows];
     TLog(@"originalDS: %@, pkey: %@ section:%zd, content rows:%zd, number of ads:%zd", self.originalTVDataSource, self.placementKey, section, numberofContentRows, self.numAdsInView);
@@ -107,6 +110,9 @@
 #pragma mark - <UICollectionViewDataSource>
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     NSInteger numberofContentRows =  [self.originalCVDataSource collectionView:collectionView numberOfItemsInSection:section];
+    if (numberofContentRows == 0) {
+        return 0;
+    }
 
     self.numAdsInView = [self.adjuster numberOfAdsInSection:section givenNumberOfRows:numberofContentRows];
     TLog(@"originalDS: %@, pkey: %@ section:%zd, content rows:%zd, number of ads:%zd", self.originalCVDataSource, self.placementKey, section, numberofContentRows, self.numAdsInView);
